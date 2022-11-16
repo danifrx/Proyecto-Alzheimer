7fe024b2db6115a27f773f392097fb544710468c
Kintans19 <rubenqp@gmail.com>
2022-11-16 10:16:52 +0100

    Responder a Daniel

--- app/src/main/java/com/multimedia/alzheimer/MainActivity.java
+++ app/src/main/java/com/multimedia/alzheimer/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
 
     // Hola kintans
 
+    //Hola Daniel
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
