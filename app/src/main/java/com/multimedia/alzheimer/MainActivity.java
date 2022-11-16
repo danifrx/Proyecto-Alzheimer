b2095d52a036eb95fc8628e8a1ef1512d84916df
PedroBao <thepedrolarin@gmail.com>
2022-11-16 10:38:13 +0100

    comentario Pedro

--- app/src/main/java/com/multimedia/alzheimer/MainActivity.java
+++ app/src/main/java/com/multimedia/alzheimer/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     //Hola Daniel ei
 
+    //FLOXOOOS!!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
