e3da8093d7de9a337828df8342ebb2b6aa0ff75e
Kintans19 <rubenqp@gmail.com>
2022-11-18 10:12:34 +0100

    Clase test

---
+++ app/src/main/java/com/multimedia/alzheimer/Test.java
@@ -0,0 +1,103 @@
+package com.multimedia.alzheimer;
+
+import java.util.Date;
+
+public class Test {
+    private int numText;
+    private int numPaciente;
+    private Date fecha;
+    private String r1;
+    private String r2;
+    private String r3;
+    private String r4;
+    private String r5;
+    private String r6;
+
+    public Test() {
+
+    }
+
+    public Test(int numText, int numPaciente, Date fecha, String r1, String r2, String r3, String r4, String r5, String r6) {
+        this.numText = numText;
+        this.numPaciente = numPaciente;
+        this.fecha = fecha;
+        this.r1 = r1;
+        this.r2 = r2;
+        this.r3 = r3;
+        this.r4 = r4;
+        this.r5 = r5;
+        this.r6 = r6;
+    }
+
+    public int getNumText() {
+        return numText;
+    }
+
+    public void setNumText(int numText) {
+        this.numText = numText;
+    }
+
+    public int getNumPaciente() {
+        return numPaciente;
+    }
+
+    public void setNumPaciente(int numPaciente) {
+        this.numPaciente = numPaciente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getR1() {
+        return r1;
+    }
+
+    public void setR1(String r1) {
+        this.r1 = r1;
+    }
+
+    public String getR2() {
+        return r2;
+    }
+
+    public void setR2(String r2) {
+        this.r2 = r2;
+    }
+
+    public String getR3() {
+        return r3;
+    }
+
+    public void setR3(String r3) {
+        this.r3 = r3;
+    }
+
+    public String getR4() {
+        return r4;
+    }
+
+    public void setR4(String r4) {
+        this.r4 = r4;
+    }
+
+    public String getR5() {
+        return r5;
+    }
+
+    public void setR5(String r5) {
+        this.r5 = r5;
+    }
+
+    public String getR6() {
+        return r6;
+    }
+
+    public void setR6(String r6) {
+        this.r6 = r6;
+    }
+}
