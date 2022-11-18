852c010bbb393c4189d1e8af6f15d26d11a21453
danifrx <danidelukas@gmail.com>
2022-11-18 10:02:00 +0100

    ventana_nota.xml HECHA FINAL

---
+++ app/src/main/java/com/multimedia/alzheimer/VentanaNota.java
@@ -0,0 +1,14 @@
+package com.multimedia.alzheimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VentanaNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_nota);
+    }
+}
