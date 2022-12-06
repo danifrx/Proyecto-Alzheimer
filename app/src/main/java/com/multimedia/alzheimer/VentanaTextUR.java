81a29c862e5beb8107cabd31390f81f3e1e5fa55
danifrx <danidelukas@gmail.com>
2022-12-06 12:44:44 +0100

    Creación de VentanaTextUR

---
+++ app/src/main/java/com/multimedia/alzheimer/VentanaTextUR.java
@@ -0,0 +1,156 @@
+package com.multimedia.alzheimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class VentanaTextUR extends AppCompatActivity {
+
+    private Button button_enviarRespuestas;
+    private Spinner spinner1;
+    private Spinner spinner2;
+    private Spinner spinner3;
+    private Spinner spinner4;
+    private Spinner spinner5;
+    private Spinner spinner6;
+    private String[] opciones1 = {"", "Circulo", "Triángulo", "Estrella", "Cuadrado"};
+    private String[] opciones2 = {"", "1.5", "6.55", "7.24", "9.58"};
+    private String[] opciones3 = {"", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+    private String[] opciones4 = {"", "2", "5", "3", "4"};
+    private String[] opciones5 = {"", "Son frutas", "Son verduras", "Son productos cárnicos", "Son vehículos"};
+    private String[] opciones6 = {"", "Hacia arriba", "Hacia la derecha", "Hacia abajo", "Hacia la izquierda"};
+
+    int ct = 0;
+    String dni, dniComprobante;
+
+    String datoSpinner1;
+    String datoSpinner2;
+    String datoSpinner3;
+    String datoSpinner4;
+    String datoSpinner5;
+    String datoSpinner6;
+    String resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_text_ur);
+
+        Intent i = getIntent();
+        dni = i.getStringExtra("DocumentoIdentidad");
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
+        spinner5 = (Spinner) findViewById(R.id.spinner5);
+        spinner6 = (Spinner) findViewById(R.id.spinner6);
+        button_enviarRespuestas = (Button) findViewById(R.id.button_enviarRespuestas);
+
+        ArrayAdapter<String> adaptador1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones1);
+        spinner1.setAdapter(adaptador1);
+
+        ArrayAdapter<String> adaptador2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones2);
+        spinner2.setAdapter(adaptador2);
+
+        ArrayAdapter<String> adaptador3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones3);
+        spinner3.setAdapter(adaptador3);
+
+        ArrayAdapter<String> adaptador4 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones4);
+        spinner4.setAdapter(adaptador4);
+
+        ArrayAdapter<String> adaptador5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones5);
+        spinner5.setAdapter(adaptador5);
+
+        ArrayAdapter<String> adaptador6 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones6);
+        spinner6.setAdapter(adaptador6);
+
+        leerDatosTest();
+        puntuacion();
+        pulsar();
+    }
+
+    public void leerDatosTest() {
+        AdminSQLiteOpenHelper adminHelper1 = new AdminSQLiteOpenHelper(this,"registro",null,1);
+        SQLiteDatabase db1 = adminHelper1.getReadableDatabase();
+        String[] columnas1 = {"dni", "resultado"};
+        String seleccion1 = "dni" + " = ?";
+        String[] condicion1= {dni};
+        Cursor c1 = db1.query("Test",columnas1,seleccion1,condicion1,null,null,null);
+        while(c1.moveToNext()) {
+            dniComprobante = c1.getString(c1.getColumnIndexOrThrow("dni"));
+        }
+        c1.close();
+        db1.close();
+    }
+
+    public void puntuacion() {
+        datoSpinner1 = spinner1.getSelectedItem().toString();
+        datoSpinner2 = spinner2.getSelectedItem().toString();
+        datoSpinner3 = spinner3.getSelectedItem().toString();
+        datoSpinner4 = spinner4.getSelectedItem().toString();
+        datoSpinner5 = spinner5.getSelectedItem().toString();
+        datoSpinner6 = spinner6.getSelectedItem().toString();
+
+        ct = 0;
+
+        if (datoSpinner1.equals("Estrella")) {
+            ct++;
+        }
+        if (datoSpinner2.equals("6.55")) {
+            ct++;
+        }
+        if (datoSpinner3.equals("Lunes")) {
+            ct++;
+        }
+        if (datoSpinner4.equals("4")) {
+            ct++;
+        }
+        if (datoSpinner5.equals("Son frutas")) {
+            ct++;
+        }
+        if (datoSpinner6.equals("Hacia la derecha")) {
+            ct++;
+        }
+
+        if (ct <= 2) {
+            resultado = "Alzheimer";
+        } else if (ct > 2 && ct <= 4) {
+            resultado = "Peligro de alzheimer";
+        } else if (ct > 4 && ct <= 6) {
+            resultado = "Buena salud mental";
+        }
+
+        //Instancio la conexión con la BBDD
+        AdminSQLiteOpenHelper adminHelper = new AdminSQLiteOpenHelper(this, "registro", null, 1);
+        //Abro la conexión de base de datos, con permisos de escritura para realizar las altas
+        SQLiteDatabase db = adminHelper.getWritableDatabase();
+
+        ContentValues valores = new ContentValues();
+
+        valores.put("dni", dni);
+        valores.put("resultado", resultado);
+        //Incremento de la del num del paciente.
+        db.insert("Test", null, valores);
+        db.close();
+    }
+
+    public void pulsar() {
+        button_enviarRespuestas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),VentanaNota.class);
+                i.putExtra("Nota", resultado);
+                startActivity(i);
+            }
+        });
+    }
+}
